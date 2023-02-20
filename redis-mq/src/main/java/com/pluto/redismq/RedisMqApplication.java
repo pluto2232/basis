4c443d9f9c0782c26c9e4e09883aa0dd9e03ec21
chenyan <271249758@qq.com>
2023-02-20 10:58:24 +0800

    init

---
+++ redis-mq/src/main/java/com/pluto/redismq/RedisMqApplication.java
@@ -0,0 +1,13 @@
+package com.pluto.redismq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisMqApplication.class, args);
+    }
+
+}
