8fd7c447bab0cb112a54ef5a281938cf90c7c566
chenyan <271249758@qq.com>
2023-02-25 17:18:11 +0800

    opencv 推流demo

---
+++ javacv-tutorials/simple-grab-push/src/main/java/com/grabpush/SimpleGrabPushApplication.java
@@ -0,0 +1,13 @@
+package com.grabpush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleGrabPushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleGrabPushApplication.class, args);
+    }
+
+}
