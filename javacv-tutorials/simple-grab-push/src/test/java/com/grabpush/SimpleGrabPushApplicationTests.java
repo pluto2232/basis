74a03cafb4cae341be4f643407e81d3bb9ab4998
chenyan <271249758@qq.com>
2023-03-24 09:10:47 +0800

    end

---
+++ javacv-tutorials/simple-grab-push/src/test/java/com/grabpush/SimpleGrabPushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grabpush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleGrabPushApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
